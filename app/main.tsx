8f2d1bb8c19cc8eaaa615f3e285935f37e44a9de
Ask Solheim <solheimen21@gmail.com>
2025-06-05 14:00:46 +0200

    gh pages once again

---
+++ app/main.tsx
@@ -0,0 +1,15 @@
+// app/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/home";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Contrast-test/", // this MUST match the repo name
+  base: "/Contrast-test/", // Your GitHub repo name
   plugins: [react()],
 });
